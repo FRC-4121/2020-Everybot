e45e563fc3b604dd4d8c7718099ecb8f0b00d82c
JonasMuhlenkamp <jonasmuhlenkamp@gmail.com>
2019-10-24 14:42:11 -0400

    Last major changes for GLSC Comp

--- src/main/java/frc/robot/commands/DriveWithJoysticksCommand.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticksCommand.java
@@ -20,7 +20,7 @@ public class DriveWithJoysticksCommand extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-  }
+  } 
 
   // Called repeatedly when this Command is scheduled to run
   @Override
